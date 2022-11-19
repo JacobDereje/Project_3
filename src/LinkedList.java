d7f943e913cf749a5b7a4c18401bf05a7e36f34a
Nob1K <bccheng1000@gmail.com>
2022-11-18 18:12:02 -0600

    everything except merge

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,225 +1,393 @@
 public class LinkedList<T extends Comparable<T>> implements List<T> {
-    private Node<T> start;
-    private Node<T> end;
+    private Node<T> start;//head of the list
+    private Node<T> end;//end of the list
     private int size;//how long is the list
     private boolean isSorted;
-    public LinkedList(){
+
+    public LinkedList() {
         this.start = null;
         this.end = null;
         this.size = 0;
         this.isSorted = true;
     }
-    @Override
-    public boolean add(T element) {
-        //return false if the element being added is null
-        if (element == null){
-            return false;
-        }
-        else{
-            //case 1: list is empty
-            if (start==null){
-                start = new Node<T>(element);
-                end = start;
-            }
-            //case2: list not empty
-            else{
-                //check if the new element breaks the sorted list
-                if (end.getData().compareTo(element)>0){
-                    isSorted = false;
-                }
-                end.setNext(new Node<T>(element));
-                end = end.getNext();
-            }
-            size++;
-            return true;
-        }
-    }
 
     @Override
+    public boolean add(T element) {
+        //check if the element being added is null
+        if (element == null) {
+            return false;
+        }
+        //case1:nothing in list
+        if (start == null) {
+            Node<T> addNode = new Node<T>(element);
+            end = addNode;
+            start = end;
+        }
+        //case2:adding to a list that already has things in it
+        else{
+            Node<T> addNode = new Node<T>(element);
+            end.setNext(addNode);
+            //check if the new added element breaks the isSorted feature
+            if (end.getData().compareTo(element) > 0) {
+                isSorted = false;
+            }
+            end = end.getNext();
+        }
+        size++;
+        return true;
+    }
+
     public boolean add(int index, T element) {
-        //check if index is out of bound or element is null
-        if (index >= size || index < 0 || element == null){
+        //check for index out of bounds or if element being added is null
+        if (index < 0 || index >= size || element == null) {
             return false;
         }
         else{
-            //case:add to the start of the list
+            //case 1:adding to start of list
             if (index == 0) {
-                start = new Node<T>(element, start);
-                if (start.getNext().getData().compareTo(element)<0) isSorted = false;
+                start = new Node<>(element, start);
+                if (element.compareTo(start.getNext().getData()) > 0) {
+                    this.isSorted = false;
+                }
                 return true;
             }
-            //case: add to any index not at the start
+            //case 2: add to any other index
             else {
                 int count = 0;
-                Node<T> trail = start;
-                Node<T> ahead = trail;
+                Node<T> ahead = start;
+                Node<T> trail = ahead;
+                //using the while loop to get to the index we want
                 while (count < index) {
                     trail = ahead;
                     ahead = ahead.getNext();
                     count++;
                 }
-                Node<T> addNode = new Node(element);
+                Node<T> addNode = new Node<T>(element);
+                //"insert" the addNode between trail and ahead
                 trail.setNext(addNode);
                 addNode.setNext(ahead);
                 size++;
-                if (trail.getData().compareTo(element) > 0 || ahead.getData().compareTo(element) < 0) {
-                    isSorted = false;
+                //check if the "insertion" mess up the sorted list
+                if (trail.getData().compareTo(addNode.getData()) > 0 || addNode.getData().compareTo(addNode.getNext().getData()) > 0) {
+                    this.isSorted = false;
                 }
-                return true;
             }
-
+            return true;
         }
     }
 
-    @Override
     public void clear() {
         start = null;
         end = null;
-        this.size = 0;
-        this.isSorted = true;
+        size = 0;
+        isSorted = true;
     }
 
-    @Override
     public T get(int index) {
-        if (index>=this.size || index < 0){
-            return null;
-        }
-        else{
-            Node loop = start;
-            for (int i=0;i<index;i++){
-                loop = loop.getNext();
-            }
-            return (T) loop.getData();
-        }
-    }
-
-    @Override
-    public int indexOf(T element) {
-        if (element == null)return -1;
-        if (isSorted){
-            Node<T> loop = start;
-            int index = 0;
-            while(loop!=null&&loop.getData().compareTo(element)<=0){
-                if (loop.getData().equals(element)){
-                    return index;
-                }
-                index++;
-                loop = loop.getNext();
-            }
-        }
-        else {
-            int index = 0;
-            Node<T> loop = start;
-            while (index < size) {
-                if (loop.getData().equals(element)) {
-                    return index;
-                }
-                index++;
-                loop = loop.getNext();
-            }
-        }
-        return -1;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return (this.size==0);
-    }
-
-    @Override
-    public int size() {
-        return this.size;
-    }
-
-    @Override
-    public void sort() {
-        if (isSorted==true) return;
-        else{
-            Node front = start;
-            Node temp;
-            Node compare;
-            while (front.getNext()!=null){
-                compare = front.getNext();
-                temp = front;
-                while (compare!=null) {
-                    if (temp.getData().compareTo(compare.getData()) > 0) {
-                        temp = compare;
-                    }
-                    compare = compare.getNext();
-                }
-                Node beforeTemp = front;
-                while(beforeTemp.getNext()!=temp){
-                    beforeTemp = beforeTemp.getNext();
-                }
-                Node afterTemp = temp.getNext();
-                temp.setNext(front);
-                beforeTemp.setNext(afterTemp);
-            }
-        }
-
-
-    }
-
-    @Override
-    public T remove(int index) {
-        if (index>=size || index <0){
+        //check if index out of bounds
+        if (index >= size || index < 0) {
             return null;
         }
         else {
+            Node<T> loop = start;
             int count = 0;
-            Node loop = start;
-            while (count < index - 1) {
+            //loop to the index we want
+            while (loop != null) {
+                if (count == index) {
+                    return loop.getData();
+                }
+                count++;
+                loop = loop.getNext();
+            }
+        }
+        return null;
+    }
+
+    public int indexOf(T element) {
+        Node<T> loop = start;
+        if (element == null) {
+            return -1;
+        }
+        if (isSorted()) {
+            int count = 0;
+            //use the sorted feature to stop at the point where we know we can
+            while (loop != null && loop.getData().compareTo(element) <= 0) {
+                if (element.equals(loop.getData())) {
+                    return count;
+                }
                 loop = loop.getNext();
                 count++;
             }
-            if (loop.getNext().getNext()==null) {
-                T ret = (T) loop.getNext().getData();
-                loop.setNext(null);
-                return ret;
-            }
-            else{
-                T ret = (T) loop.getNext().getData();
-                loop.setNext(loop.getNext().getNext());
-                return ret;
-            }
+            return -1;
         }
-    }
-
-    @Override
-    public void equalTo(T element) {
-
-    }
-
-    @Override
-    public void reverse() {
-        Node newList = null;
-        Node ptr = start;
-        while (ptr!=null){
-            Node temp = ptr;
-            ptr = ptr.getNext();
-            temp.setNext(newList);
-            newList = temp;
-        }
-        start.setNext(newList);
-    }
-
-    @Override
-    public void merge(List<T> otherList) {
-        if (otherList==null) return;
         else{
-            LinkedList<T> other = (LinkedList<T>) otherList;
-            sort();
-            other.sort();
-
+            int count = 0;
+            //loop till we find the element we want
+            while (loop != null) {
+                if (!element.equals(loop.getData())) {
+                    loop = loop.getNext();
+                    count++;
+                }
+                else {
+                    return count;
+                }
+            }
+            return -1;
         }
     }
 
-    @Override
-    public void pairSwap() {
-
+    public boolean isEmpty() {
+        return (size==0);
     }
 
-    @Override
+    public int size() {
+        Node<T> counter = start;
+        size = 0;
+        while (counter != null) {
+            counter = counter.getNext();
+            size++;
+        }
+        return size;
+    }
+
+    public void sort() {
+        //if the list is sorted don't do anything
+        if (isSorted()) {
+            return;
+        } 
+        else {
+            Node<T> front;
+            Node<T> compare;
+            //use selection sort to sort the list
+            for (front = start; front.getNext() != null; front = front.getNext()) {
+                Node<T> lowest = front;
+                for (compare = front.getNext(); compare != null; compare = compare.getNext()) {
+                    if (compare.getData().compareTo(lowest.getData()) < 0) {
+                        lowest = compare;
+                    }
+                }
+                T temp = lowest.getData();
+                lowest.setData(front.getData());
+                front.setData(temp);
+            }
+        }
+        isSorted = true;
+    }
+
+    public T remove(int index) {
+        isSorted = true;
+        Node<T> loop = start;
+        Node<T> trail = null;
+        T target;
+        int count = 0;
+        //check for invalid index
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        else {
+            //remove the first element
+            if (index == 0) {
+                target = start.getData();
+                start = start.getNext();
+            }
+            //general case
+            else {
+                //loop until reach the desired index
+                while (count < index) {
+                    trail = loop;
+                    loop = loop.getNext();
+                    count++;
+                }
+                //remove the target and reconnect the elements before and after the target
+                target = loop.getData();
+                trail.setNext(loop.getNext());
+            }
+            size--;
+        }
+        //check if the list is still sorted and update accordingly
+        loop = start;
+        if (size <= 1) {
+            isSorted = true;
+        }
+        else {
+            while (loop != null && loop.getNext() != null) {
+                if (loop.getData().compareTo(loop.getNext().getData()) > 0) {
+                    isSorted = false;
+                }
+                loop = loop.getNext();
+            }
+        }
+        return target;
+    }
+
+    public void equalTo(T element) {
+        Node<T> ptr = start;
+        Node<T> trail = null;
+        size = 0;
+        if (isSorted) {
+            if (start.getData() == null) {
+                return;
+            }
+            //use the while loop to find the element and keep it
+            //use the isSorted feature to find it faster
+            while (ptr != null) {
+                if (element.compareTo(ptr.getData()) > 0) {
+                    start = ptr.getNext();
+                }
+                else if (element.compareTo(ptr.getData()) < 0) {
+                    end = trail;
+                    return;
+                }
+                else{
+                    size++;
+                }
+                trail = ptr;
+                ptr = ptr.getNext();
+            }
+        }
+        else {
+            // eliminate elements that don't equal to the one we want
+            while (start != null && start.getData().compareTo(element) != 0) {
+                trail = ptr;
+                start = start.getNext();
+                ptr = start;
+            }
+            if (start == null) {
+                isSorted = true;
+                return;
+            }
+            //loop through the list and recount the size
+            while (ptr != null) {
+                if (element.compareTo(ptr.getData()) == 0) size++; 
+                else {
+                    trail.setNext(ptr.getNext());
+                    ptr = trail;
+                }
+                trail = ptr;
+                ptr = ptr.getNext();
+            }
+        }
+        isSorted = true;
+    }
+
+    public void reverse() {
+        Node<T> n1 = start;
+        Node<T> n2 = start;
+        int i = 0;
+        while (i < size / 2) {
+            int index = size - 1 - (2 * i);
+            int j = 0;
+            while (j < index) {
+                n2 = n2.getNext();
+                j++;
+            }
+            T temp = n1.getData();
+            n1.setData(n2.getData());
+            n2.setData(temp);
+            n1 = n1.getNext();
+            n2 = n1;
+            i++;
+        }
+        //if list was sorted, reversing would make it not sorted
+        if (isSorted) {
+            isSorted = false;
+        }
+        //check whether the reversed list was sorted if the list was not sorted previously
+        else {
+            n1 = start;
+            while (n1 != null && n1.getNext() != null) {
+                if (n1.getData().compareTo(n1.getNext().getData()) > 0) {
+                    isSorted = false;
+                    return;
+                }
+                n1 = n1.getNext();
+            }
+            isSorted = true;
+        }
+    }
+
+    public void merge(List<T> otherList) {
+        LinkedList<T> other = (LinkedList<T>) otherList;
+        sort();
+        other.sort();
+        size = 0;
+        Node<T> ptr1 = start;
+        Node<T> ptr2 = other.start;
+        Node<T> temp = null;
+        Node<T> ptr = temp;
+        if (ptr2 == null) {
+            return;
+        }
+        if (ptr1 == null) {
+            this.start = other.start;
+            return;
+        }
+        while (ptr1 != null || ptr2 != null) {
+            if (ptr1 == null) {
+                ptr.setNext(ptr2);
+                break;
+            } else if (ptr2 == null) {
+                ptr.setNext(ptr1);
+                break;
+            } else {
+                if (ptr1.getData().compareTo(ptr2.getData()) < 0) {
+                    if (temp == null) {
+                        temp = ptr1;
+                        ptr = temp;
+                    } else {
+                        ptr.setNext(ptr1);
+                        ptr = ptr.getNext();
+                    }
+                    ptr1 = ptr1.getNext();
+                } else {
+                    if (temp == null) {
+                        temp = ptr2;
+                        ptr = temp;
+                    } else {
+                        ptr.setNext(ptr2);
+                        ptr = ptr.getNext();
+                    }
+                    ptr2 = ptr2.getNext();
+                }
+            }
+        }
+        size += other.size;
+        this.start = temp;
+    }
+
+    public void pairSwap() {
+        Node<T> ptr = start;
+        while (ptr != null && ptr.getNext() != null) {
+            //reverse the two elements in every set of two elements
+            Node<T> next = ptr.getNext();
+            T temp = ptr.getData();
+            ptr.setData(next.getData());
+            next.setData(temp);
+            ptr = next.getNext();
+        }
+        //check if sorted
+        ptr = start;
+        while (ptr != null && ptr.getNext() != null) {
+            if (ptr.getData().compareTo(ptr.getNext().getData()) > 0) {
+                isSorted = false;
+                return;
+            }
+            ptr = ptr.getNext();
+        }
+        isSorted = true;
+    }
+
+
+    public String toString() {
+        Node<T> loop = start;
+        String output = "";
+        while (loop != null) {
+            output += loop.getData() + "\n";
+            loop = loop.getNext();
+        }
+        return output;
+    }
+
     public boolean isSorted() {
         return isSorted;
     }
